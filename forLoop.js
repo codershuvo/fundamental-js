65bea7bb74b4a960ab6754827b5cb6302fe5a237
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-18 00:01:44 +0600

    function added

---
+++ forLoop.js
@@ -0,0 +1,14 @@
+
+
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i); 
+}
+
+
+// array all element for forloop 
+var friendsName = ['somir', 'ahad', 'kalam', 'jamal', 'balam', 'palam', 'talam']; 
+for (var i = 0; i < friendsName.length; i++) { 
+    var element = friendsName[i]; 
+    console.log(element);
+}
---
+++ switch.js
@@ -0,0 +1,23 @@
+
+num = 5; 
+switch (num) {
+    case 1000:
+        console.log('i am 1k')
+        break;
+    case 100:
+        console.log('i am 1 hundred')
+        break;
+    case 20:
+        console.log('i am 20')
+        break;
+    case 5:
+        console.log('i am 5th')
+        break;
+    case 10:
+        console.log('i am 10')
+        break;
+
+    default:
+        console.log('I do not no who you are');
+        break;
+}
