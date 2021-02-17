06d1401901626eb78b5b00c97d5a842928f60aac
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-17 23:28:08 +0600

    array update

---
+++ array.js
@@ -0,0 +1,8 @@
+// array
+var friendsAge = [15, 17, 14, 16]; 
+
+var sonaliAge = friendsAge[2]; // check no. of position
+friendsAge[1] = 22; // set position
+var position = friendsAge.indexOf(14); // check position
+
+console.log(position); 
