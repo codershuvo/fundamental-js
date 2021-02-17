fa4d4fe13a8c8ff90249f6c3f59312e93d69b6c3
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-17 23:47:30 +0600

    loop added

--- array.js
+++ array.js
@@ -5,4 +5,13 @@ var sonaliAge = friendsAge[2]; // check no. of position
 friendsAge[1] = 22; // set position
 var position = friendsAge.indexOf(14); // check position
 
-console.log(position); 
+friendsAge.push(26);  // add last position of value
+console.log(friendsAge.length) // koto gula element 
+friendsAge.pop(); //remove element for last position
+friendsAge.shift(); // remove element for first
+friendsAge.unshift(12); // add element for first 
+
+var teaLine = ['somir', 'ahad', 'kalam', 'jamal', 'balam', 'palam', 'talam']; 
+var part = teaLine.slice(2, 5); // 2 index theke 5 er age porjonto dhora
+console.log(part); 
+console.log(teaLine);
